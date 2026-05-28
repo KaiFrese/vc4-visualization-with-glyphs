d56ea3583d2d375b430518af1d8e960da7a68183
Kai Frese <fresekai@gmail.com>
2026-05-28 14:46:21 +0200

    implemented dimension selection

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,34 @@
 
 document.addEventListener('DOMContentLoaded', async () => {
     const fileReader = new FileReader();
+    const legendSVG = document.getElementById('legend-svg');
+    const chartSVG = document.getElementById('chart-svg');
     let data = [];
 
+    // -1 -> none
+    //  0 -> car name
+    //  1 -> manufacturer
+    //  2 -> consumption
+    //  3 -> cylinders
+    //  4 -> displacement
+    //  5 -> horsepower
+    //  6 -> weight
+    //  7 -> acceleration
+    //  8 -> year
+    //  9 -> origin
+    let selectedDimensions = {
+        xAxis: -1,
+        yAxis: -1,
+        size: -1,
+        color: -1,
+        shape: -1,
+    };
+
     document.getElementById('file-input').addEventListener('change', () => {
-        console.log(document.getElementById('file-input').files[0]);
-        fileReader.readAsText(document.getElementById('file-input').files[0]);
+        const fileInput = document.getElementById('file-input');
+        if (fileInput.files.length > 0) {
+            fileReader.readAsText(fileInput.files[0]);
+        }
     });
 
     fileReader.addEventListener('load', () => {
@@ -25,5 +48,23 @@ document.addEventListener('DOMContentLoaded', async () => {
             row[7] = parseFloat(row[7]);
             row[8] = parseFloat(`19${row[8]}`);
         });
+
+        drawChart(chartSVG, data, selectedDimensions);
     });
+
+    addChangeHandlerToSelections(selectedDimensions);
 });
+
+function addChangeHandlerToSelections(selectedDimensions) {
+    Object.keys(selectedDimensions).forEach((property) =>
+        document
+            .getElementById(`${property}-select`)
+            .addEventListener(
+                'change',
+                (event) =>
+                    (selectedDimensions[property] = parseInt(
+                        event.target.value,
+                    )),
+            ),
+    );
+}
